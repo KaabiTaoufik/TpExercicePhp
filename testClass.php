71dd3647911b743a920379d5f2e5d77a06d8f8bb
unknown <taoufikkaabi@insat.u-carthage.tn>
2021-04-17 10:54:20 +0100

    modif

--- testClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-//include_once 'classes/personne.php';
-include_once 'autoload.php';
-$personne = new Personne('sellaouti', 'aymen', 38);
-$voiture = new Voiture();
-$voiture->whoAmI();
-echo $personne;
--- userInfos.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = 'aymen';
-
-$studentName = 'sara';
-$studentNameSection = 'GL2';
-
-$GLOBALS['name']="aymen";
-$GLOBALS['section']= "GL2"
-?>
